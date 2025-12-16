5a43921f0d27726f9f032614747405fda0f9eb93
ZikPin <ziya.gulu365@gmail.com>
2025-12-16 23:11:40 +0100

    removing editing route

--- src/app/story/story-page-routing.module.ts
+++ src/app/story/story-page-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from "@angular/core";
-import { StoryPageEditComponent } from "./story-page-edit/story-page-edit.component";
 import { StoryPageViewComponent } from "./story-page-view/story-page-view.component";
 import { Route, RouterModule } from "@angular/router";
 
 export const routes: Route[] = [
-    {
-        path: 'book-page/:id/edit',
-        component: StoryPageEditComponent
-    },
     {
         path: 'book-page/:id/view',
         component: StoryPageViewComponent
